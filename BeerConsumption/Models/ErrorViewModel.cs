9f1512818f47abc969585328c2cb838da7ad79d0
Linas Wilkialis <Linas.Wilkialis@macaw.nl>
2019-10-14 16:12:52 +0300

    Add project files.

---
+++ BeerConsumption/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeerConsumption.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
